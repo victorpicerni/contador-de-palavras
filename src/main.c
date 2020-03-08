e9a40932bfd52fe9130e7f001ec501a58c3542d6
victorpicerni <victorhugopicerni@gmail.com>
2020-03-07 21:58:05 -0300

    adicionei alguns comandos de comparacao

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+ 
 /* Contador de palavras
  *
  * Este programa recebera uma serie de caracteres representando palavras em sua
@@ -10,13 +11,51 @@
 int main() {
 
   char c;
-
-  c = 1;
+  c = 0;
+  int cont = 1;
+  char umantes;
+  char doisantes;
 
   while (c != '\n') {
     scanf("%c", &c);
+//Iniciando os comandos para contagem de palavras
+
+//Contando palavras separadas por spaço, contando uma nova palavra a partir da primeira letra de cada  	
+	if (c != ' ' && umantes == ' ')
+		cont = cont + 1;
+
+//contando palavras que não sao separadas por espaço, mas contém uma virgula no meio
+	if (c !=' ' & umantes == ',')
+		cont = cont + 1;
+
+//contando palavras que não sao separadas por espaço, mas contém um ponto no meio
+	if (c !=' ' & umantes == '.')
+		cont = cont + 1;
+
+//contando palavras que não sao separadas por espaço, mas contém um traço no meio
+	if (c !=' ' & umantes == '-')
+		cont = cont + 1;
+
+//retirando a contagem a partir do ponto, visto que numeros com ponto e com virgula contam apenas como um. (para um caso mais geral, seria necessário incluir os numeros de 0 a 9)
+	if (c =='.' && umantes == '3') 
+		cont = cont - 1;
+
+	if (c ==',' && umantes == '0')
+		cont = cont - 1;
+//retirando a contagem excedente das reticências
+	if (umantes == '.' && doisantes == '.' && c== '.')
+		cont = cont - 1	;
+//retirando a contagem do ultimo espaço como palavra.
+	if (c == '\n' && umantes ==' ')
+		cont = cont - 1;
+//retirando a contagem do ultimo ponto como palavra.
+	if (c == '\n' && umantes =='.')
+		cont = cont - 1;
+  	//variaveis guardadas para comparações dos loops
+	umantes= c;
+  	doisantes= umantes;
   }
 
-  printf("1\n");
+  printf("%d\n", cont);
   return 0;
 }
